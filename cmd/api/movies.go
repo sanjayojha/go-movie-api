7bb5a1f76f903221db1f8bfe6006af72a0ffc32c
Sanjay Ojha <ojha.phy21@gmail.com>
2025-04-25 16:49:32 +0530

    validation on filters

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -184,3 +184,31 @@ func (app *application) deleteMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request) {
+
+	var input struct {
+		Title  string
+		Geners []string
+		data.Filters
+	}
+
+	qs := r.URL.Query()
+	v := validator.New()
+
+	input.Title = app.readString(qs, "title", "")
+	input.Geners = app.readCSV(qs, "genres", []string{})
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
+	input.Filters.Sort = app.readString(qs, "sort", "id")
+
+	input.Filters.SortSafelist = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
+
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
+
+}
