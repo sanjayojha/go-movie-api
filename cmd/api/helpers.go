8f277473b58264fa63770c08358b1dfc14ae03b9
Sanjay Ojha <ojha.phy21@gmail.com>
2026-06-28 13:29:36 +0530

    added json decoder and validation

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -39,5 +42,62 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+
+	// Use http.MaxBytesReader() to limit the size of the request body to 1,048,576 bytes (1MB).
+	r.Body = http.MaxBytesReader(w, r.Body, 1_048_576)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+	if err != nil {
+		var (
+			syntaxError           *json.SyntaxError
+			unmarshalTypeError    *json.UnmarshalTypeError
+			invalidUnmarshalError *json.InvalidUnmarshalError
+			maxBytesError         *http.MaxBytesError
+		)
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("body contains badly-formed JSON (at character %d)", syntaxError.Offset)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return fmt.Errorf("body contains badly-formed JSON")
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("body contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+
+		case errors.Is(err, io.EOF):
+			return errors.New("body must not be empty")
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			field := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("body contains unknown key %s", field)
+
+		case errors.As(err, &maxBytesError):
+			return fmt.Errorf("body must not be larger than %d bytes", maxBytesError.Limit)
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		default:
+			return err
+		}
+	}
+
+	// Call Decode() again, using a pointer to an empty anonymous struct as the destination. If the request body only contained a single JSON value this will return an io.EOF error. So if we get anything else, we know that there is additional data in the request body and we return our own custom error message.
+	err = dec.Decode(&struct{}{})
+
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must only contain a single JSON value")
+	}
 	return nil
 }
--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,8 +1,10 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type Runtime int
@@ -15,4 +17,35 @@ func (r Runtime) MarshalJSON() ([]byte, error) {
 	quotedJSONValue := strconv.Quote(jsonValue)
 
 	return []byte(quotedJSONValue), nil
+}
+
+// Implement a UnmarshalJSON() method on the Runtime type so that it satisfies the json.Unmarshaler interface. IMPORTANT: Because UnmarshalJSON() needs to modify the receiver (our Runtime type), we must use a pointer receiver for this to work correctly. Otherwise, we will only be modifying a copy (which is then discarded when this method returns).
+
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	errInvalidRuntimeFormat := errors.New("invalid runtime format")
+
+	// We expect that the incoming JSON value will be a string in the format  "<runtime> mins", and the first thing we need to do is remove the surrounding  double quotes from this string. If we can't unquote it, then we return the  errInvalidRuntimeFormat error.
+	unquotedJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return errInvalidRuntimeFormat
+	}
+
+	// Split the string to isolate the part containing the number.
+	parts := strings.Split(unquotedJSONValue, " ")
+
+	// Sanity check the parts of the string to make sure it was in the expected format. If it isn't, we return the errInvalidRuntimeFormat error again.
+	if len(parts) != 2 || parts[1] != "mins" {
+		return errInvalidRuntimeFormat
+	}
+
+	// Otherwise, parse the string containing the number into an int. Again, if this fails return the ErrInvalidRuntimeFormat error.
+	i, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return errInvalidRuntimeFormat
+	}
+
+	// Convert the int to a Runtime type and assign this to the receiver. Note that we use the * operator to dereference the receiver (which is a pointer to a Runtime type) in order to set the underlying value of the pointer.
+	*r = Runtime(i)
+
+	return nil
 }
