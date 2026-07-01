856ceaa0cdd8ac410c6499abfb612b727d59a9fd
Sanjay Ojha <ojha.phy21@gmail.com>
2026-07-01 13:09:00 +0530

    input data validation

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"movieapi.sanjayojha.dev/internal/data"
+	"movieapi.sanjayojha.dev/internal/validator"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,6 +23,25 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 		app.badRequestResponse(w, r, err)
 		return
 	}
+
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+
+	// validating data
+	// Initialize a new Validator instance.
+	v := validator.New()
+
+	data.ValidateMovie(v, movie)
+
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
 
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"time"
+
+	"movieapi.sanjayojha.dev/internal/validator"
+)
 
 type Movie struct {
 	ID        int       `json:"id"`
@@ -10,4 +14,22 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitzero"` // movie runtime in minutes
 	Version   int       `json:"version"`          // start with 1, updated when move info updated
 	CreatedAt time.Time `json:"-"`
+}
+
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= time.Now().Year(), "year", "must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+
+	// Note that we're using the Unique helper in the line below to check that all values in the input.Genres slice are unique.
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
 }
