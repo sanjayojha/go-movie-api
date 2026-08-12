5e6894e60fb56ea34e0ea5e97dde84918d8dd0c4
Sanjay Ojha <ojha.phy21@gmail.com>
2026-08-12 16:05:35 +0530

    creating authentication token

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,6 +26,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// genrate token
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
 	// Wrap the router with the panic recovery middleware.
 	return app.recoverPanic(app.rateLimit(router))
 }
