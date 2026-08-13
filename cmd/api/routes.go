fa9c3f1ad4a184d253dec537ff7b2e5300261a31
Sanjay Ojha <ojha.phy21@gmail.com>
2026-08-13 13:33:47 +0530

    permissions tables and middlewares

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -16,12 +16,12 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 
 	// Add the route for the /v1/movies endpoints.
-	// Use the requireActivatedUser() middleware on /v1/movies** endpoints.
-	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requireActivatedUser(app.createMovieHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requireActivatedUser(app.showMovieHandler))
-	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.requireActivatedUser(app.updateMovieHandler))
-	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requireActivatedUser(app.deleteMovieHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/movies", app.requireActivatedUser(app.listMovieHandler))
+	// Use the requirePermission() middleware on each of the /v1/movies** endpoints
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.requirePermission("movies:write", app.createMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.requirePermission("movies:read", app.showMovieHandler))
+	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.requirePermission("movies:write", app.updateMovieHandler))
+	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.requirePermission("movies:write", app.deleteMovieHandler))
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.requirePermission("movies:read", app.listMovieHandler))
 
 	// Add the route for the /v1/users endpoints.
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
---
+++ internal/data/permissions.go
@@ -0,0 +1,61 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"slices"
+	"time"
+)
+
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+// Define a Permissions slice, which we will use to hold the permission codes (such as "movies:read" and "movies:write") for a single user.
+type Permissions []string
+
+func (p Permissions) Include(code string) bool {
+	return slices.Contains(p, code)
+}
+
+// Returns all permission codes for a specific user in a Permissions slice
+func (m PermissionModel) GetAllForUser(userID int) (Permissions, error) {
+	query := `
+	SELECT permissions.code
+	FROM permissions
+	INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
+	INNER JOIN users ON users_permissions.user_id = users.id
+	WHERE users.id = $1
+	`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, userID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var permissions Permissions
+	for rows.Next() {
+		var permission string
+
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+
+		permissions = append(permissions, permission)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+
+}
