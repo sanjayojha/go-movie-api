ad67bce73c2f1723fa19c30b459e764cc25265fd
Sanjay Ojha <ojha.phy21@gmail.com>
2026-08-14 14:00:03 +0530

    record http status code in metrics

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"slices"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -15,6 +16,12 @@ import (
 	"movieapi.sanjayojha.dev/internal/data"
 	"movieapi.sanjayojha.dev/internal/validator"
 )
+
+type metricsResponseWriter struct {
+	wrapped       http.ResponseWriter
+	statusCode    int
+	headerWritten bool
+}
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -214,12 +221,39 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	})
 }
 
+func newMetricsResponseWriter(w http.ResponseWriter) *metricsResponseWriter {
+	return &metricsResponseWriter{
+		wrapped:    w,
+		statusCode: http.StatusOK,
+	}
+}
+
+func (mw *metricsResponseWriter) Header() http.Header {
+	return mw.wrapped.Header()
+}
+
+func (mw *metricsResponseWriter) WriteHeader(statusCode int) {
+	mw.wrapped.WriteHeader(statusCode)
+
+	if !mw.headerWritten {
+		mw.statusCode = statusCode
+		mw.headerWritten = true
+	}
+}
+
+func (mw *metricsResponseWriter) Write(b []byte) (int, error) {
+	mw.headerWritten = true
+	return mw.wrapped.Write(b)
+}
+
 func (app *application) metrics(next http.Handler) http.Handler {
 	// Initialize the new expvar variables when the middleware chain is first built.
 	var (
 		totalRequestsReceived           = expvar.NewInt("total_requests_received")
 		totalResponsesSent              = expvar.NewInt("total_responses_sent")
 		totalProcessingTimeMicroseconds = expvar.NewInt("total_processing_time_μs")
+
+		totalResponseSentByStatus = expvar.NewMap("total_responses_sent_by_status")
 	)
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -228,11 +262,15 @@ func (app *application) metrics(next http.Handler) http.Handler {
 		// Use the Add() method to increment the number of requests received by 1.
 		totalRequestsReceived.Add(1)
 
-		// Call the next handler in the chain.
-		next.ServeHTTP(w, r)
+		mw := newMetricsResponseWriter(w)
+
+		// Call the next handler in the chain using the new metricsResponseWriter as the http.ResponseWriter value.
+		next.ServeHTTP(mw, r)
 
 		// On the way back up the middleware chain, increment the number of responses sent by 1.
 		totalResponsesSent.Add(1)
+
+		totalResponseSentByStatus.Add(strconv.Itoa(mw.statusCode), 1)
 
 		// Calculate the number of microseconds since we began to process the request, then increment the total processing time by this amount.
 		duration := time.Since(start).Microseconds()
