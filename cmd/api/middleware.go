dcb37186105d615d6f5bddfc8239a69077fd723f
Sanjay Ojha <ojha.phy21@gmail.com>
2026-08-14 10:26:24 +0530

    preflight cors request support added

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -191,10 +191,21 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
 
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
 		origin := r.Header.Get("Origin")
 		if origin != "" {
 			if slices.Contains(app.config.cors.trustedOrigins, origin) {
 				w.Header().Set("Access-Control-Allow-Origin", origin)
+
+				// handling preflight request
+				if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+					w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+					w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+					w.WriteHeader(http.StatusOK)
+					return
+				}
 			}
 
 		}
